e33784a0fb147607de5b2a1407e44763d0609947
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-11 12:00:20 -0700

    adding print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print_comb’
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+
+x = 0;
+
+while (x <= 9)
+{
+	putchar(x + '0');
+
+	if (x == 9)
+	{
+		putchar('\n');
+       
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	
+
+	x++;
+}
+
+
+return (0);
+
+}
