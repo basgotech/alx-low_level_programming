a6a1872ad0bcb5574f3cdc6595697dee9e2dc667
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-11 10:26:02 -0700

    adding last digit project

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,32 +5,31 @@
 /**
  * main - Entry point
  *
- * Description: 'the program's used for get last digit number and procees..'
+ * Description: 'calculating the last num of decimals'
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
+int n;
 
-	int lastDigit;
+int lastDigit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
 
-	if (lastDigit > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	}
-	else if (lastDigit < 6 && lastDigit != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n", n,  lastDigit);
-	}
-	return (0);
+if (lastDigit > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+}
+else if (lastDigit < 6 && lastDigit != 0)
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+}
+else
+{
+	printf("Last digit of %d is %d and is 0\n", n,  lastDigit);
+}
+return (0);
 }
