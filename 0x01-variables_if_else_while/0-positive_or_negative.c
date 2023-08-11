dc2acffd17d50e9dee11baac4aa730d24254b3d7
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-11 01:46:44 -0700

    adding posetive or negative project in c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* main - function identify the number negetive , zero and posetive */
-
-/* return 0 success */
+/**
+ * main - Entry point for check the random number negative , zero or posetive
+ * Return:0
+ */
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n>0){
-	printf("%d is positive\n",n);
+if (n > 0)
+{
+	printf("%d is positive\n", n);
 }
-else if(n<0){
-	printf("%d is negative\n",n);
+else if (n < 0)
+{
+	printf("%d is negative\n", n);
 }
-else{
-	printf("%d is zero\n",n);
+else
+{
+	printf("%d is zero\n", n);
 }
 return (0);
 }
