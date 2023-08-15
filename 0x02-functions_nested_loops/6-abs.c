3b2a0c592bcd99937e81632f177bb56b3e69c53d
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-15 13:14:26 -0700

    adding  that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * _abs - Checks if a character is greater than 0.
+ *   * @num: The character to be checked.
+ *    *
+ *     * Return: absolute value.
+ *
+ */
+int _abs(int num)
+{
+if (num > 0)
+{
+return (num);
+}
+else
+{
+return (-num);
+}
+}
