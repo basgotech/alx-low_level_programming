935c9cee86213e4bc698361a787023b1bcf36d2e
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-17 06:16:15 -0700

    adding function that draws a diagonal line on the terminal.

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  * print_diagonal - Checks if a character is lowercase.
+ *   * @c: The character to be checked.
+ *    *
+ *
+ */
+void print_diagonal(int c)
+{
+
+int y, z;
+
+if (c < 1)
+{
+_putchar('\n');
+}
+else
+{
+for (y = 0; y < c; y++)
+{
+for (z = 0; z <= y; z++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+
+}
