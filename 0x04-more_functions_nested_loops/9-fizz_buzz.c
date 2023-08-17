974e7d2947afbca3337f744167fde8973fc71fd5
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-17 07:35:45 -0700

    adding fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - fizz buzz
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+int y;
+
+for (y = 1; y <= 100; y++)
+{
+if (y % 3 == 0 && y % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (y % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (y % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", y);
+}
+
+}
+
+printf("\n");
+
+return (0);
+
+}
