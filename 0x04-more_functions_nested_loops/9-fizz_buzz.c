f3d655e36a355815bccae7ebcac12d416c3bf4e4
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-17 07:48:24 -0700

    adding The “Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,27 @@ for (y = 1; y <= 100; y++)
 {
 if (y % 3 == 0 && y % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (y % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else if (y % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else
 {
-printf("%d ", y);
+printf("%d", y);
 }
 
+if (y == 100)
+continue;
+printf(" ");
+
 }
+
 
 printf("\n");
 
