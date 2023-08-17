0371798d2d3eba7858ed1812e512874bc64e7894
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-17 00:26:01 -0700

    adding isupper project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  * _isupper - Checks if a character is lowercase.
+ *   * @c: The character to be checked.
+ *    *
+ *     * Return: 1 if letter, 0 otherwise.
+ *
+ */
+int _isupper(int c)
+{
+
+
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+
+}
