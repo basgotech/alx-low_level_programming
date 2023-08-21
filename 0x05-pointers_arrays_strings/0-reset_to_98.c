29382547e6c66bf77f6de15af69fe35eccd90695
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-20 21:27:35 -0700

    adding 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * * reset_to_98 - used to chnage the value of n -
+ *  * by usning pionters
+ *   * @n: Hold memory address of variable n
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
