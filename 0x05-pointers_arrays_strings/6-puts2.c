e353bf7eb0c63fbeecfdff05ac10c4b3588cc056
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-21 09:41:13 -0700

    adding prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,23 +5,24 @@
 *@str: store string value to be printed.
 *
 */
-
 void puts2(char *str)
 {
-int count;
+int index_val = 0, len = 0;
+
 char *i = str;
-char *p;
 while (*i != '\0')
 {
-count++;
+len++;
 i++;
 }
 
-
-for (p = str; *p; p += 2)
+for (index_val = 0; index_val < len; index_val = index_val + 2)
 {
-_putchar(*p);
+_putchar(str[index_val]);
 }
+
+
+
 _putchar('\n');
 
 }
