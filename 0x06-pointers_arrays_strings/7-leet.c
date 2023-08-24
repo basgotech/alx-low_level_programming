286eb5568bef61c5665480bb93cea8333cefe707
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-24 08:20:53 -0700

    adding leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,19 +5,22 @@
  */
 char *leet(char *s)
 {
-	char *r = s;
-	char a[] = { 'a', 'e', 'o', 't', 'l' };
-	char n[] = { 4, 3, 0, 7, 1 };
-	int i = 0;
+char *r = str;
+char a[] = {'a', 'e', 'o', 't', 'l'};
+char n[] = {4, 3, 0, 7, 1};
+int i;
 
-	while (*s)
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*s == a[i] || *s == a[i] - 32)
-				*s = n[i] + '0';
-		}
-		s++;
-	}
-	return (r);
+for (; *str; str++)
+{
+for (i = 0; i < 5; i++)
+{
+if (*str == a[i] || *str == a[i] - 32)
+{
+*str = n[i] + '0';
+break;
+}
+}
+}
+
+return (r);
 }
