de52e37e31a07987ec0d3843d1becd5c010d273c
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-24 10:55:35 -0700

    adding function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,26 +2,16 @@
 
 /**
 * rot13 - rotate characters 13 places
-* @str: string
+* @s: string
 * Return: string rotated
 */
-#include "main.h"
-
-/**
-* rot13 - rotate characters 13 places
-* @str: string
-* Return: string rotated
-*/
-
 char *rot13(char *s)
 {
     int i = 0;
     while (s[i] != '\0')
     {
-        if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
-            s[i] += 13;
-        else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-            s[i] -= 13;
+        if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+            s[i] = (s[i] - 'A' + 13) % 26 + 'A';
         i++;
     }
     return (s);
