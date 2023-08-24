1a52ad8013879be41ee3045bf277a9a2eb4d4dc5
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-24 04:25:53 -0700

    adding a function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  * _strcmp - functions that compare strings.
+ *   * @s1: The destination string copy goes to.
+ *    *@s2: src of strings to be copied
+ *     * Return:  Return the result in numeric.
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+for (; *s1 && *s2 && *s1 == *s2; s1++, s2++)
+{
+
+}
+
+
+
+return (*s1 - *s2);
+}
