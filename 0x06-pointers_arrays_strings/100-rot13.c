ce8d15537eebd7fa587f3d568ecff1eb8d17a960
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-24 10:32:41 -0700

    function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,22 +2,28 @@
 
 /**
 * rot13 - rotate characters 13 places
-* @s: string
+* @str: string
 * Return: string rotated
 */
 
-char *rot13(char *str) {
-  int i;
-  char lower[] = "abcdefghijklmnopqrstuvwxyz";
-  char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char *rot13(char *str)
+{
+int i;
+char lower[] = "abcdefghijklmnopqrstuvwxyz";
+char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  for (i = 0; str[i]; i++) {
-    if (str[i] >= 'a' && str[i] <= 'z') {
-      str[i] = lower[(str[i] - 'a' + 13) % 26];
-    } else if (str[i] >= 'A' && str[i] <= 'Z') {
-      str[i] = upper[(str[i] - 'A' + 13) % 26];
-    }
-  }
+for (i = 0; str[i]; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = lower[(str[i] - 'a' + 13) % 26];
+}
+else if
+(str[i] >= 'A' && str[i] <= 'Z')
+{
+str[i] = upper[(str[i] - 'A' + 13) % 26];
+}
+}
 
-  return str;
+return (str);
 }
