b5ab227382717bba55e4e069ee918ecf79025084
Basliel Tegegn <basgolilo5@gmail.com>
2023-08-30 00:01:56 -0700

    adding a function that prints a string, followed by a new line.

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main.h - Header file containing declarations for all functions
+*       0x08. C - Recursion
+*/
+
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
