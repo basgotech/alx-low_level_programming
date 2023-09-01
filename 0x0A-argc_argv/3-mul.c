87dd1a2041c27d601e457655471e2e422bb364bc
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-01 07:07:43 -0700

    adding a program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,6 +14,11 @@ int main(int argc, char *argv[])
 int i;
 
 int result = 1;
+if (argc != 3)
+{
+printf("%s\n", "Error");
+return (1);
+}
 for (i = 1; i < argc; i++)
 {
 result *=  strtol(argv[i], NULL, 10);
