05682e6fcaaee0e6939acc83184c367a9e44d487
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-05 05:36:46 -0700

    adding str dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,41 +12,32 @@ int _strlen(char *s)
 return ((*s) ? _strlen(++s) + 1 : 0); /* return length of s*/
 }
 
-/**
- * _memcpy -  function that copies memory area.
- * @dest: A pointer to the destination memory block.
- * @src: A pointer to the source memory block.
- * @n: The number of bytes to be copied.
- * Return: will print the contents of the buffer2 array .
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-unsigned char *dptr = (unsigned char *) dest;
-unsigned char *sptr = (unsigned char *) src;
-unsigned int nn = n;
-while (nn--)
-{
-*dptr++ = *sptr++;
-}
 
-return (dest);
-
-}
 /**
  * _strdup- a pointer to a newly allocated space in memory
- *@src: string
+ *@str: string
  *Return: returns a pointer to a newly allocated space in memory
  */
-char *_strdup(char *src)
+char *_strdup(char *str)
 {
-int len;
-char *dst;
+char *dup;
+unsigned int i, len;
 
-len = _strlen(src) + 1;
-dst = malloc(len);
-if (dst != my_null)
-{
-_memcpy(dst, src, len);
-}
-return (dst);
+i = 0;
+len = 0;
+
+if (str == NULL)
+return (NULL);
+
+len = _strlen(str);
+
+dup = malloc(sizeof(char) * (len + 1));
+
+if (dup == NULL)
+return (NULL);
+
+while ((dup[i] = str[i]) != '\0')
+i++;
+
+return (dup);
 }
