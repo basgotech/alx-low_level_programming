04c8469b4decf8aec3ad915864f38843ea425c30
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-05 06:38:15 -0700

    adding string concatinate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -44,26 +44,39 @@ return ((*s) ? _strlen(++s) + 1 : 0); /* return length of s*/
 char *str_concat(char *s1, char *s2)
 {
 
-int len1, len2, len;
 char *new_str;
-if (s1 == NULL || s2 == NULL)
-{
-return (NULL);
-}
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 len1 = _strlen(s1);
 len2 = _strlen(s2);
-len = len1 + len2 + 1;
-new_str = malloc(len);
+
+new_str = malloc(sizeof(char) * (len1 + len2 + 1));
 if (new_str == NULL)
-{
 return (NULL);
+
+i = 0;
+j = 0;
+
+if (s1)
+{
+while (i < len1)
+{
+new_str[i] = s1[i];
+i++;
+}
 }
 
-_memcpy(new_str, s1, len1);
-_memcpy(new_str + len1, s2, len2);
-
-new_str[len - 1] = '\0';
+if (s2)
+{
+while (i < (len1 + len2))
+{
+new_str[i] = s2[j];
+i++;
+j++;
+}
+}
+new_str[i] = '\0';
 
 return (new_str);
+
 }
