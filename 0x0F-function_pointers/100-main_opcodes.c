319e6c58a50949455dbe3eb679f472d4a123b54e
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-13 16:15:01 -0700

    adding main codes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
+*_atoi - a function that convert a string to an integer.
+*@s: file manipulated data type.
+*Return: return atoi vale
+*/
+int _atoi(char *s)
+{
+int sign = 1;
+unsigned int num = 0;
+
+do {
+if (*s == '-')
+{
+sign *= -1;
+}
+else if (*s >= '0' && *s <= '9')
+{
+num = (num * 10) + (*s - '0');
+}
+else if (num > 0)
+{
+break;
+}
+s++;
+} while (*s);
+
+return (num *sign);
+}
+/**
  * main- Entry point
  *@argc: the number of parameters.
  *@argv: the parameeters in the case the number ob bytes.
@@ -16,7 +44,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	n = atoi(argv[1]);
+	n = _atoi(argv[1]);
 	if (n < 0)
 	{
 		printf("Error\n");
@@ -26,7 +54,7 @@ int main(int argc, char *argv[])
 	for (i = 0; i < n; i++)
 	{
 		printf("%02hhx", *((char *)main + i));
-		if (i < n - 1)
+		if (i < n - 3)
 			printf(" ");
 		else
 			printf("\n");
