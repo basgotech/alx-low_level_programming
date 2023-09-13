59250208cd5e92b34c167bc73e05fc6876c098e3
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-13 16:20:10 -0700

    adding main codes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	for (i = 0; i < n; i++)
 	{
 		printf("%02hhx", *((char *)main + i));
-		if (i < n - 3)
+		if (i < n - 1)
 			printf(" ");
 		else
 			printf("\n");
