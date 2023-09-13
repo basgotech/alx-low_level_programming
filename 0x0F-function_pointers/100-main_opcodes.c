2ba9f7925d535f5ae7a467b51ff870688f95a88f
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-13 11:42:54 -0700

    add

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main -  a program that prints the opcodes of its own main function.
- * @argc: argument count
- * @argv: argument vector
- * Return: o if success
- */
 int main(int argc, char *argv[])
 {
 int i;
 if (argc != 2)
 {
-printf("Error\n");
+printf("Usage: ./100-main_opcodes.c number_of_bytes\n");
 exit(1);
 }
 
 int num_bytes = atoi(argv[1]);
-
-if (num_bytes < 0)
-{
-printf("Error\n");
-exit(2);
-}
 
 void *main_addr = (void *)main;
 
@@ -33,5 +21,5 @@ printf("%02x ", *(unsigned char *)(main_addr + i));
 
 printf("\n");
 
-return (0);
+return 0;
 }
