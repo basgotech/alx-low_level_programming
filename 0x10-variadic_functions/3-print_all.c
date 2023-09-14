74d0f9aeda43a3c2a354d9980bc1d66ff9a7961e
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-14 11:21:27 -0700

    adding print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,49 +4,49 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints all the arguments that are passed to it in any format
+ * print_all - a function that prints strings
  * @format: format of the argument
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, j, sep = 0;
-	char *str;
-	char c_args[] = "cifs";
-	va_list args;
+unsigned int k, l, sep = 0;
+char *str;
+char c_args[] = "cifs";
+va_list args;
 
-	va_start(args, format);
-	i = 0;
-	while (format && format[i])
-	{
-		j = 0;
-		while (c_args[j])
-		{
-			if (format[i] == c_args[j] && sep)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(args, int)), sep = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int)), sep = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double)), sep = 1;
-				break;
-			case 's':
-				str = va_arg(args, char *), sep = 1;
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
-		} i++;
-	} va_end(args), printf("\n");
+va_start(args, format);
+k = 0;
+while (format && format[k])
+{
+l = 0;
+while (c_args[l])
+{
+if (format[k] == c_args[l] && sep)
+{
+printf(", ");
+break;
+} l++;
+}
+switch (format[k])
+{
+case 'c':
+printf("%c", va_arg(args, int)), sep = 1;
+break;
+case 'i':
+printf("%d", va_arg(args, int)), sep = 1;
+break;
+case 'f':
+printf("%f", va_arg(args, double)), sep = 1;
+break;
+case 's':
+str = va_arg(args, char *), sep = 1;
+if (!str)
+{
+printf("(nil)");
+break;
+}
+printf("%s", str);
+break;
+} k++;
+} va_end(args), printf("\n");
 }
