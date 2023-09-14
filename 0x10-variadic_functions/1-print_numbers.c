bf9553684de630e7294dafd5439380002b47d11f
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-14 04:04:29 -0700

    adding a function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_umbers - a function that prints numbers,
+ * print_numbers - a function that prints numbers,
  * followed by a new line.
  * @separator: This is a pointer to a string that will be used
  * to separate the numbers
@@ -11,21 +11,18 @@
  * Return: numbers separated by commas.
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...) {
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
 unsigned int i, k;
 va_list args;
 va_start(args, n);
 
 k = (unsigned int)n;
-if (separator == NULL)
-{
-separator = "";
-}
 
 for (i = 0; i < k; i++)
 {
 int number = va_arg(args, int);
-if (i == k-1)
+if (i == k - 1)
 {
 separator = "";
 }
