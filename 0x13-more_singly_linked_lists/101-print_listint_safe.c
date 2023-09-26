9118f81a72d0a5e744c703e433f52ef5a9aa32b6
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-26 10:21:52 -0700

    adding print list safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,36 +6,35 @@
  * @head: A pointer to the head of the linked list.
  * Return: The number of nodes in the list.
  */
-size_t print_listint_safe(const listint_t *head) {
-    const listint_t *slow;
-    const listint_t *fast;
-    size_t node_count;
-    node_count = 0;
-    slow = head;
-    fast = head;
+size_t print_listint_safe(const listint_t *head)
+{
+const listint_t *temp_list = NULL;
+const listint_t *slow_list = NULL;
+size_t len_count = 0;
+size_t new_list;
 
-    while (fast != NULL && fast->next != NULL) {
-        printf("%d\n", slow->n);
-        node_count++;
-        slow = slow->next;
-        fast = fast->next->next;
-
-
-        if (slow == fast) {
-            printf("%d\n", slow->n);
-            node_count++;
-
-
-            exit(98);
-        }
-    }
-
-
-    while (slow != NULL) {
-        printf("%d\n", slow->n);
-        node_count++;
-        slow = slow->next;
-    }
-
-    return node_count;
+temp_list = head;
+while (temp_list)
+{
+printf("[%p] %d\n", (void *)temp_list, temp_list->n);
+len_count++;
+temp_list = temp_list->next;
+slow_list = head;
+new_list = 0;
+while (new_list < len_count)
+{
+if (temp_list == slow_list)
+{
+printf("-> [%p] %d\n", (void *)temp_list, temp_list->n);
+return (len_count);
+}
+slow_list = slow_list->next;
+new_list++;
+}
+if (!head)
+{
+exit(98);
+}
+}
+return (len_count);
 }
