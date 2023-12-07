556c607713bcc606879739e6a51f9fd9028326c2
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-07 11:05:47 -0800

    adding del dnodeint

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,46 +1,42 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes the node at index
  * @head: pointer to the address of the head of the doubly linked list
  * @index: index of the node that should be deleted
- *
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current, *temp;
-    unsigned int i;
-
-    if (head == NULL || *head == NULL)
-        return -1;
-
-    current = *head;
-
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return 1;
-    }
-
-    for (i = 0; i < index && current != NULL; i++)
-    {
-        current = current->next;
-    }
-
-    if (current == NULL)
-        return -1;
-
-    temp = current->prev;
-    temp->next = current->next;
-
-    if (current->next != NULL)
-        current->next->prev = temp;
-
-    free(current);
-
-    return 1;
+dlistint_t *current_pos, *temp_swap;
+unsigned int i;
+if (head == NULL || *head == NULL)
+{
+return (-1);
+}
+current_pos = *head;
+if (index == 0)
+{
+*head = current_pos->next;
+if (*head != NULL)
+{(*head)->prev = NULL;
+}
+free(current_pos);
+return (1);
+}
+for (i = 0; i < index && current_pos != NULL; i++)
+{
+current_pos = current_pos->next;
+}
+if (current_pos == NULL)
+{return (-1);
+}
+temp_swap = current_pos->prev;
+temp_swap->next = current_pos->next;
+if (current_pos->next != NULL)
+{
+current_pos->next->prev = temp_swap;
+}
+free(current_pos);
+return (1);
 }
