513c56745bcfe92846d69f1eae9679432a4a7783
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-07 10:12:02 -0800

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t counter = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        counter++;
-    }
-
-    return counter;
+size_t counter = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+counter++;
+}
+return (counter);
 }
