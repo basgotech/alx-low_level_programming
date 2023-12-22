9a9b9d82f35f7b6b946628622f8e436fcac30e1a
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-22 03:14:41 -0800

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,12 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 hash_node_t *new_hash;
 char *cp_val;
 unsigned long int index, x;
-if (ht == NULL || key == NULL)
-{
-if (*key == '\0' || value == NULL)
+if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 {
 return (0);
-}
 }
 cp_val = strdup(value);
 if (cp_val == NULL)
