c7b3119b77ea98d582feaa3fe46d4371b1e9c9b4
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-22 00:32:37 -0800

    adding first task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "HASH_ALGO.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ * Return: If an error occurs - NULL.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int x;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+    {
+        return (NULL);
+    }
+	
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+    {
+        return (NULL);
+    }
+    
+	for (x = 0; x < size; x++)
+    {
+        hash_table->array[x] = NULL;
+    }
+
+	return (hash_table);
+}
