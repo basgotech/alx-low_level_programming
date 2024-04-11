2ea8e90b8b8eee8ccccffc6b2063d7af2524d361
Basliel Tegegn <basgolilo5@gmail.com>
2024-04-11 05:34:30 -0700

    add

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,48 +1,61 @@
 #include "search_algos.h"
 
 /**
- * binary_search - binary search 
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * custom_recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @arr: input array
+ * @arr_size: size of the array
+ * @target: value to search for
+ * Return: index of the target value
  */
-int binary_search(int *array, size_t size, int value)
+int custom_recursive_search(int *arr, size_t arr_size, int target)
 {
-int low;
-int high;
-int mid;
-size_t i;
-if (array == NULL)
-{
-return -1;
-}
-low = 0;
-high = size - 1;
+	size_t half = arr_size / 2;
+	size_t i;
 
-while (low <= high)
-{
-mid = low + (high - low) / 2;
+	if (arr == NULL || arr_size == 0)
+		return (-1);
 
-printf("Searching in array: ");
-for (i = low; i <= (size_t)high; i++)
-{
-printf("%d ", array[i]);
-}
-printf("\n");
+	printf("Searching in array");
 
-if (array[mid] == value)
+	for (i = 0; i < arr_size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", arr[i]);
+
+	printf("\n");
+
+	if (half && arr_size % 2 == 0)
+		half--;
+
+	if (target == arr[half])
+		return ((int)half);
+
+	if (target < arr[half])
+		return (custom_recursive_search(arr, half, target));
+
+	half++;
+
+	return (custom_recursive_search(arr + half, arr_size - half, target) + half);
+}
+
+/**
+ * binary_search - calls custom_recursive_search to return
+ * the index of the number
+ *
+ * @arr: input array
+ * @arr_size: size of the array
+ * @target: value to search for
+ * Return: index of the target value
+ */
+int binary_search(int *arr, size_t arr_size, int target)
 {
-return (mid);
-}
-else if (array[mid] < value)
-{
-low = mid + 1;
-}
-else
-{
-high = mid - 1;
-}
-}
-return (-1);
+	int index;
+
+	index = custom_recursive_search(arr, arr_size, target);
+
+	if (index >= 0 && arr[index] != target)
+		return (-1);
+
+	return (index);
 }
